8395b58c88a34da9bae7ede7c970bbd1497823e6
Douglas Silva <douglasifbaianosb@gmail.com>
2022-09-30 10:02:53 -0300

    Adicionando novo console.log e testando o pull request para a branch dev

--- src/server.mjs
+++ src/server.mjs
@@ -3,4 +3,4 @@ import express from "express";
 const app = express();
 
 console.log("listening on port 8080");
-console.log("pull request on branch dev");
+console.log("pull request on branch dev with github actions");
